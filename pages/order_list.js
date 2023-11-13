0e2c0fc7559db0e1564825426e74f8399d20cf53
Whitie017 <90831172+Whitie017@users.noreply.github.com>
2023-11-13 16:01:08 +0700

    [13/11] update UI

--- pages/order_list.js
+++ pages/order_list.js
@@ -1,44 +1,44 @@
 import DefaultLayout from "@/components/Layout";
-import { AiOutlineSearch } from "react-icons/ai";
-import { DatePicker, Divider, Tabs, Pagination,Table } from "antd";
+import { AiOutlineSearch, AiOutlinePlus } from "react-icons/ai";
+import { DatePicker, Divider, Tabs, Pagination, Table } from "antd";
 
 
-const  columns = [
-    {
-      title: "Mã đơn hàng",
-      dataIndex: "order_id",
-      key: "order_id",
-    },
-    {
-      title: "Tên sản phẩm",
-      dataIndex: "product_name",
-      key: "product_name",
-    },
-    {
-      title: "Mã khách hàng",
-      dataIndex: "customer_id",
-      key: "customer_id",
-    },
-    {
-      title: "Địa chỉ",
-      dataIndex: "address",
-      key: "address",
-    },
-    {
-      title: "Ngày đặt hàng",
-      dataIndex: "date",
-      key: "date",
-    },
-    {
-      title: "Giá sản phẩm",
-      dataIndex: "price",
-      key: "price",
-    },
-    {
-      title: "Tình trạng",
-      dataIndex: "status",
-      key: "status",
-    },
+const columns = [
+  {
+    title: "Mã đơn hàng",
+    dataIndex: "order_id",
+    key: "order_id",
+  },
+  {
+    title: "Tên sản phẩm",
+    dataIndex: "product_name",
+    key: "product_name",
+  },
+  {
+    title: "Mã khách hàng",
+    dataIndex: "customer_id",
+    key: "customer_id",
+  },
+  {
+    title: "Địa chỉ",
+    dataIndex: "address",
+    key: "address",
+  },
+  {
+    title: "Ngày đặt hàng",
+    dataIndex: "date",
+    key: "date",
+  },
+  {
+    title: "Giá sản phẩm",
+    dataIndex: "price",
+    key: "price",
+  },
+  {
+    title: "Tình trạng",
+    dataIndex: "status",
+    key: "status",
+  },
 ];
 
 const data = [
@@ -84,7 +84,7 @@ const data = [
   },
 ];
 
-  
+
 
 const table_template = () => {
   return (
@@ -124,33 +124,43 @@ export default function Order() {
   ];
   return (
     <DefaultLayout>
-      {/* //Search bar */}
-      <div class="relative w-80 mb-7">
-        <input
-          class="border w-full border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-xs font-bold focus:outline-none"
-          type="search"
-          name="search"
-          placeholder="Tìm kiếm đơn hàng"
-
-        />
-
-        <button type="submit" class="absolute right-0 top-0 mt-3 mr-4">
-          <AiOutlineSearch class="text-slate-400 text-lg" />
-        </button>
+      <div class="float-left">
+        <p>
+          <span class="text-2xl font-bold mr-3">Đơn hàng</span>
+          <span class="font-bold text-slate-500	">15 đơn hàng được tìm thấy</span>
+        </p>
       </div>
+
 
       {/* //Category */}
       <div class="block border px-10 py-10">
+        {/* //Search bar */}
         <div class="inline-block w-full">
-          <div class="float-left">
-            <p>
-              <span class="text-2xl font-bold mr-3">Đơn hàng</span>
-              <span class="font-bold text-slate-500	">15 đơn hàng được tìm thấy</span>
-            </p>
+          <div class="relative float-left w-80 mb-5">
+            <input
+              class="border w-full border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-xs focus:outline-none"
+              type="search"
+              name="search"
+              placeholder="Tìm kiếm đơn hàng"
+            />
+
+            <button type="submit" class="absolute right-0 top-0 mt-3 mr-4">
+              <AiOutlineSearch class="text-slate-400 text-lg" />
+            </button>
           </div>
           <div class="float-right">
-            <DatePicker />
-          </div>
+              <DatePicker />
+            </div>
+        </div>
+
+        <div class="inline-block w-full">
+            <button>
+              <span>
+                <AiOutlinePlus />
+                
+              </span>
+            </button>
+
         </div>
 
         <div>
@@ -159,7 +169,7 @@ export default function Order() {
 
 
 
-        
+
       </div>
     </DefaultLayout>
   );
