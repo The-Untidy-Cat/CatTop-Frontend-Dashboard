5f0f4198e780f6b9f5ab3d5c1e12cdf80f310a60
Whitie017 <90831172+Whitie017@users.noreply.github.com>
2023-11-16 21:31:45 +0700

    [16/11] update button

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import DefaultLayout from "@/components/Layout";
 import { AiOutlineSearch } from "react-icons/ai";
 import { DatePicker, Divider, Tabs, Pagination,Table } from "antd";
-
+import { App } from "@/pages/test.js";
 
 export default function Home() {
   return (
     <DefaultLayout>
-      
+
         
     </DefaultLayout>
   );
