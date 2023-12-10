f57782e8c388d197b7f9925106e9c7c216bfdcc8
Phuong Anh <tpa05403@gmail.com>
2023-12-10 13:02:54 +0700

    pull

--- pages/thu.js
+++ pages/thu.js
@@ -1,45 +1,211 @@
-import { Button } from "antd";
+import { Button, Form, Input, Radio, Select, InputNumber, Table } from "antd";
+import { getAllCustomer } from "@/services/customer";
+import { getAllProduct } from "@/services/product";
 import { searchRead } from "@/services/search_read";
-import { useEffect, useState } from "react";
-import { useAmp } from "next/amp";
 import { api } from "@/utils/axios";
-import { getUnlimitAllOrder } from "@/services/order";
+const { useRouter } = require("next/router");
+const { useState, useEffect } = require("react");
 
-export default function Apps() {
-  const limit = 5;
-  const [products, setProducts] = useState([]);
-  const [keyword, setKeyword] = useState(null);
-  const [length, setLength] = useState(0);
-  const [offset, setOffset] = useState(0);
+const NewProduct = (id, form) => {
+  const [value, setValue] = useState("");
   const [loading, setLoading] = useState(false);
-  // const getData = async () => {
-  //   setLoading(true);
-  //   try {
-  //     const response = await searchRead({
-  //       model: "Order",
-  //       domain: keyword ? [["name", "=", keyword]] : [],
-  //       fields: ["order_id", "customer_id","employee_id", "create_at","payment_state","state"],
-  //       limit,
-  //       offset,
-  //       relation: ["customer:id, first_name","employee:id, first_name"],
-  //     });
-  //     console.log(response);
-  //     // setBrands(response?.records.map((item) => ({ ...item, key: item.id })));
-  //     // setLength(response?.length);
-  //     // setOffset(response?.offset);
-  //   } catch (error) {
-  //     console.log("error", error);
-  //   }
-  //   setLoading(false);
-  // };
-
-  const getData = async () => {
-    const response = await api.get('/dashboard/orders', {
-      params: {
-        limit: 1000,
-      },
-    });
-    console.log(response);
+  const [customers, setCustomer] = useState([])
+  const [products, setProducts] = useState([]);
+  const [dataSource, setDataSource] = useState([
+    {
+      key: '',
+      name: '',
+    },
+  ]);
+  const [count, setCount] = useState(0);
+  const handleAdd = () => {
+    const newData = {
+      key: count,
+      name: ``,
+    };
+    setDataSource([...dataSource, newData]);
+    setCount(count + 1);
   };
-  return (<Button onClick={getData}>ABC</Button>);
+  const handleDelete = (key) => {
+    const newData = dataSource.filter((item) => item.key !== key);
+    setDataSource(newData);
+  };
+  const getCustomer = async () => {
+    try {
+      const response = await getAllCustomer()
+      console.log(response)
+      setCustomer(response?.records.map(
+        (item) => (
+          {
+            value: item.id,
+            label: item.last_name + " " + item.first_name
+          })))
+    }
+    catch (e) {
+      console.log(e)
+    }
+  }
+
+  const columns = [
+    {
+      title: "Sản phẩm",
+      dataIndex: "sanpham",
+      key: "sanpham",
+      render: () => (
+        <Form.Item
+          className="m-0"
+          rules={[
+            {
+              required: true,
+              message: "Vui lòng chọn một giá trị",
+            },
+          ]}
+        >
+          <Select
+            showSearch
+            placeholder="Chọn sản phẩm"
+            optionFilterProp="children"
+            filterOption={filterOption}
+            options={products}
+            className="w-full"
+          />
+        </Form.Item>
+      ),
+    },
+    {
+      title: 'Số lượng',
+      dataIndex: "soluong",
+      key: "soluong",
+      render: () => (
+        <Form.Item
+          className="m-0"
+          rules={[
+            {
+              required: true,
+              message: "Vui lòng chọn một giá trị",
+            },
+          ]}
+        >
+          <InputNumber
+            min={1}
+            max={10}
+            defaultValue={1}
+            className="w-1/2"
+          />
+        </Form.Item>
+      ),
+    },
+    {
+      title: '',
+      render: (record) => (
+        <Form.Item
+          className="m-0"
+        >
+          <Button
+            onClick={() => handleDelete(record.key)}
+          >-
+          </Button>
+        </Form.Item>
+      ),
+    }
+  ]
+  const getProducts = async () => {
+    try {
+      // const response = await getAllProduct()
+      const response = await searchRead({
+        model: "Product",
+        domain: [],
+        fields: ["id", "name"],
+      })
+      setProducts(response?.records.map(
+        (item) => (
+          {
+            value: item.id,
+            label: item.name
+          }
+        )
+      ))
+    }
+    catch (e) {
+      console.log(e)
+    }
+  }
+  useEffect(() => {
+    getCustomer()
+    getProducts()
+  }, [])
+
+
+  const onChange = (e) => {
+    console.log('radio checked', e.target.value);
+    setValue(e.target.value);
+  }
+  const filterOption = (input, option) =>
+    (option?.label ?? '').toLowerCase().includes(input.toLowerCase());
+  return (
+    <Form
+      autoComplete="off"
+      className="flex flex-col w-full gap-2"
+    >
+      <div className="grid grid-cols-2 gap-2">
+        <div className="flex flex-col gap-2 w-full">
+          <p className="m-0">Tên khách hàng</p>
+          <Form.Item
+            className="m-0"
+            rules={[
+              {
+                required: true,
+                message: "Vui lòng chọn một giá trị",
+              },
+            ]}
+          >
+            <Select
+              showSearch
+              placeholder="Chọn khách hàng"
+              optionFilterProp="children"
+              filterOption={filterOption}
+              options={customers}
+            />
+          </Form.Item>
+        </div>
+        <div className="col-span-2">
+          <p className="m-0">Phương thức thanh toán</p>
+          <Form.Item
+            className="m-0"
+          >
+            <Radio.Group onChange={onChange} value={value}>
+              <Radio value={'chuyen-khoan'}>Chuyển khoản</Radio>
+              <Radio value={'tien-mat'}>Tiền mặt</Radio>
+            </Radio.Group>
+          </Form.Item>
+        </div>
+        <div className="flex flex-col gap-2 w-full">
+          <p>Sản phẩm</p>
+          <Button
+            onClick={handleAdd}
+            type="primary"
+            className="bg-primary/[.7] hover:bg-primary"
+          >
+            Thêm sản phẩm
+          </Button>
+
+          <Table
+            columns={columns}
+            dataSource={dataSource}
+            pagination={false}
+          />
+           {/* <Table dataSource={dataSource} columns={columns} /> */}
+        </div>
+        <div className="col-span-2">
+          <Form.Item className="m-0 mt-2">
+            <Button type="primary" className="w-full hover:bg-primary bg-primary/[.8]" htmlType="submit">
+              Hoàn thành
+            </Button>
+          </Form.Item>
+        </div>
+      </div>
+    </Form >
+  );
 }
+
+export default NewProduct;
